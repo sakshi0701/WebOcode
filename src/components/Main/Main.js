cfec415aa475d04c27a63fd6f832c3a80a358590
Sakshi Dwivedi <sakshithedwivedi@gmail.com>
2021-06-27 01:13:08 +0530

    commit

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -5,7 +5,7 @@ const Main = () => {
     return (
         <div className="main" id="home">
             <div className="tagline" style={{ color: "#00d9ffdc" }}>
-                <h3>Let the games begin! <span>Next generation gaming is here!</span><h4 className="sub-tagline">Enjoy the thrill with our exclussive collection of games which are G.O.A.T.!<br />To experience the gaming get along with us by logging in here!</h4></h3>
+                <h3>Let the games begin! <span>Next generation gaming is here!</span><h4 className="sub-tagline">Enjoy the thrill with our exclusive collection of games which are G.O.A.T.!<br />To experience the gaming get along with us by logging in here!</h4></h3>
                 <div className="btn">
                     <button className="login-btn">Login</button>
                 </div>
