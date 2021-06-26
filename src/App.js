4858a5bb263d4691f21185c08418d97b27f19aad
Sakshi Dwivedi <sakshithedwivedi@gmail.com>
2021-06-26 13:18:04 +0530

    Footer added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+
+import Footer from './components/footer/Footer';
+
 import './App.css';
 
 function App() {
   return (
-    <div>
+    <div className="App">
       <h1>Hello world</h1>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { FaInstagram, FaFacebook, FaTwitter, FaWhatsapp } from 'react-icons/fa'
+import './footer.css'
+
+const Footer = () => {
+    return (
+        <div className="footer-main">
+            <div className="footer-icon">
+                <svg height="80" width="80">
+                    <circle cx="27" cy="40" r="25" stroke="white" strokeWidth="2" fill="none"></circle>
+                </svg>
+                <FaInstagram className="i main-icon"/>
+            </div>
+
+            <div className="footer-icon">
+                <svg height="80" width="80">
+                    <circle cx="27" cy="40" r="25" stroke="white" strokeWidth="2" fill="none"></circle>
+                </svg>
+                <FaFacebook className="i main-icon"/>
+            </div>
+
+            <div className="footer-icon">
+                <svg height="80" width="80">
+                    <circle cx="27" cy="40" r="25" stroke="white" strokeWidth="2" fill="none"></circle>
+                </svg>
+                <FaTwitter className="i main-icon"/>
+            </div>
+
+            <div className="footer-icon">
+                <svg height="80" width="80">
+                    <circle cx="27" cy="40" r="25" stroke="white" strokeWidth="2" fill="none"></circle>
+                </svg>
+                <FaWhatsapp className="i main-icon"/>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
